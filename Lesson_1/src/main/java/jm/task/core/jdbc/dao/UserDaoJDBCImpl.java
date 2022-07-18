e2adf6ab9468afb42fabf4ea1126d84d0d2a56ed
TNX0H <dimanmityan@yandex.ru>
2022-07-18 22:19:13 +0300

    1.1.3

---
+++ Lesson_1/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,80 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    private static final Connection conn = Util.getConnection();
+
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public void createUsersTable() {
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastname VARCHAR(255), age INT)");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dropUsersTable() {
+        try (Statement statement = conn.createStatement()){
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        try (PreparedStatement ps = conn.prepareStatement("INSERT INTO users (name,lastname, age) VALUES (?,?,?)")){
+            ps.setString(1,name);
+            ps.setString(2,lastName);
+            ps.setByte(3,age);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void removeUserById(long id) {
+        try (PreparedStatement ps = conn.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            ps.setLong(1,id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+
+        try (ResultSet resultSet = conn.createStatement().executeQuery("SELECT * FROM users")) {
+            while (resultSet.next()){
+                User user = new User(resultSet.getString("name"), resultSet.getString("lastname"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate("TRUNCATE TABLE users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
